7d9320e19e6ca420a12047d38ed0a0c7f52fc48f
Karina Trajano Ceoni <san.raposa@gmail.com>
2020-01-02 21:54:14 +0000

    Factory Pattern

--- PizzaFactory/PizzaFactory/Pizzas/ChicagoClamPizza.cs
+++ PizzaFactory/PizzaFactory/Pizzas/ChicagoClamPizza.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PizzaFactory.Pizzas
+﻿namespace PizzaFactory.Pizzas
 {
     public class ChicagoClamPizza : Pizza
     {
